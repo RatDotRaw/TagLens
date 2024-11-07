95d77ba25da20a6b648ccc474947fce7de7d9045
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-07 15:20:40 +0100

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,6 +1,6 @@
 import re
 import logging
-from typing import Set
+from typing import List, Set
 from crawler import File
 
 class Hashtag:
@@ -79,3 +79,29 @@ def read_and_count_hashtags(file: File) -> Set[Hashtag]:
     return hashtags
 
     # get unique hashtags and create a Hashtag object foreach one linking to the file where it was found
+
+def read_all_and_count_hashtags(files: List[File]) -> Set[Hashtag]:
+    """
+    Reads all files in a list and counts the occurrences of each hashtag.
+    
+    Args:
+        files (List[File]): The list of files to read and count hashtags in.
+    
+    Returns:
+        set: A set of unique hashtags found in all files.
+    """
+    all_hashtags: Set[Hashtag] = []
+    for file in files:
+        all_hashtags.extend(read_and_count_hashtags(file))
+
+    all_unique_hashtags: set[Hashtag] = set()
+    for hashtag in all_hashtags:
+        if not any(h.name == hashtag.name for h in all_unique_hashtags):
+            all_unique_hashtags.add(hashtag)
+        else:
+            # find and increase count and sources of the duplicate hashtag
+            for h in all_unique_hashtags:
+                if h.name == hashtag.name:
+                    h.count += hashtag.count
+                    h.sources.update(hashtag.sources)
+    return all_unique_hashtags
