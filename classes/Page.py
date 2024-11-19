decb1629a6ef67d9c8c95884c856c70563643e72
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-19 14:45:23 +0100

    feat(Page.py): Implement completion logic for Page class

--- classes/Page.py
+++ classes/Page.py
@@ -21,10 +21,43 @@ class Page:
         TODO: (add methods here as they are implemented)
     """
     def __init__(self, file: File):
+        self.name: str = file.name # TODO: make a function that cleans up the name of the file.
         self.file: File = file
         self.tag_count: int = 0
-        self.hashtags: Hashtag = set() # TODO: fill hashtgas in
-        self.word_count: int = 0 # TODO: count words and fill word count
+        self.hashtags: Hashtag = set() 
+        self.word_count: int = 0
         self.sentiment_tags = set() # TODO: fill sentiment tags in
         self.category = {} # TODO: analyse and fill in page category.
 
+        self.date: datetime = crawler.get_page_date(file.name) # date in the files filename # creation date of file is not trustworthy because of git.
+
+        # update not set variables with data from file
+        self.update_hashtags()
+        self.Update_word_count()
+
+    def update_hashtags(self) -> tuple[set[Hashtag], int]:
+        """
+        Reads the content of the page, counts the number of unique hashtags and fills them into the set self.hashtags.
+
+        Returns:
+            hashtag_list (list): A list of unique hashtags found in the page content.
+        """
+        # update hashtag count
+        content = reader.read_file(self.file.fullpath) 
+        hashtag_list = reader.find_hashtags(content) # get list of hashtags from content
+        self.tag_count = len(hashtag_list) # update tag count
+
+        self.hashtags = reader.read_and_count_hashtags(self.file)
+
+        return self.hashtags, self.tag_count
+
+    def Update_word_count(self) -> int:
+        """
+        Reads the content of the page, counts the number of words and update self.word_count.
+
+        Returns:
+            int: The number of words in the page content.
+        """
+        content = reader.read_file(self.file.fullpath)
+        self.word_count = reader.count_words(content)
+        return self.word_count
