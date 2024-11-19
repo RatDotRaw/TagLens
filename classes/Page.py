09641dd048829629d2fafbe2b366481a367c4e0d
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-19 12:10:59 +0100

    feat(page): add date var

--- classes/Page.py
+++ classes/Page.py
@@ -1,4 +1,6 @@
+import datetime
 from classes.File import File
+import tools.crawler as crawler
 from classes.Hashtag import Hashtag
 
 class Page:
@@ -12,3 +14,5 @@ class Page:
         self.word_count: int = 0 # TODO: count words and fill word count
         self.sentiment_tags = set() # TODO: fill sentiment tags in
         self.category = {} # TODO: analyse and fill in page category.
+
+        self.date: datetime = crawler.get_page_date(file.name) # date in the files filename # creation date of file is not trustworthy because of git.
