2b76d02c37a39c67d67b93b8c142e9d35e35563b
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-20 12:31:53 +0100

    Fix(analysis): limit tensor size.

--- tools/analysis.py
+++ tools/analysis.py
@@ -3,7 +3,7 @@ from typing import List
 from transformers import pipeline
 
 logging.info("loading text-classification models...")
-classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None)
+classifier = pipeline(task="text-classification", model="SamLowe/roberta-base-go_emotions", top_k=None, max_length=512)
 
 # sentences = ["I slept pretty good, but not long enough. I woke up with my shark still between my legs. which was a surprise! that means I probably didn't sleep on my belly."]
 # model_outputs = classifier(sentences)
