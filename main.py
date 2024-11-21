bb8432dd6282209be1af6c2b456419b5f31657f0
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-21 10:49:42 +0100

    wip!: push main.py code

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import tools.crawler as crawler
 import tools.reader as reader
+from classes.Page import Page
+import logging
 
 logseq_base_path = "/home/stafd/Documents/git/logseq"
 
+logging.getLogger().setLevel(logging.INFO)
+
 def read_hashtags():
     # gather list of all files to be scanned
-    journal_files = crawler.crawl(f"{logseq_base_path}/pages")
-    pages_files = crawler.crawl(f"{logseq_base_path}/journals")
+    journal_files = crawler.crawl(f"{logseq_base_path}/journals")
+    pages_files = crawler.crawl(f"{logseq_base_path}/pages")
     # get all unique hashtags, their count, and source file
     hashtag_set = reader.read_all_and_count_hashtags(journal_files)
     hashtag_set = hashtag_set.union(reader.read_all_and_count_hashtags(pages_files))
@@ -36,9 +40,34 @@ def read_hashtags():
     #     print("first appearance date:", hashtag.first_appearance_date)
     #     print("last appearance date:", hashtag.last_appearance_date)
 
+def read_pages():
+    # gather list of all files to be scanned
+    journal_files = crawler.crawl(f"{logseq_base_path}/journals")
+    pages_files = crawler.crawl(f"{logseq_base_path}/pages")
+
+    logging.info("Reading pages")
+    pages_set: set[Page] = set()
+    for file in journal_files:
+        page = Page(file)
+        
+        # set Page.journal_entry to true for all journal entries
+        page.journal_entry = True
+
+        pages_set.add(page)
+    logging.info(f"Total pages loaded: ${len(pages_set)}")
+    
+    # Set sentiments for only journal entries
+    logging.info("Updating sentiment tags for journal entries")
+    for page in pages_set:
+        print(f"page: {page.name}, journal entry: {page.journal_entry}")
+        if page.journal_entry:
+            logging.info(f"Updating sentiment tags for {page.name}")
+            page.Update_sentiment_tags(threshold=0.3)
 
 def main():
-    hashtag_set = readHashtags()
+    # hashtag_set = read_hashtags()
+    page_set = read_pages()
+    
 
 if __name__ == "__main__":
     main()
