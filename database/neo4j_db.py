4e786df90686f79dbdb5cd894ebc98589cf78e28
RatDotRaw <33757638+RatDotRaw@users.noreply.github.com>
2024-11-23 12:48:20 +0100

    Feat(neo4j): create insert_hashtag func

--- database/neo4j_db.py
+++ database/neo4j_db.py
@@ -2,9 +2,7 @@ import os
 from dotenv import load_dotenv
 import logging
 from neo4j import GraphDatabase
-# from classes.Page import Page
-# from classes.Hashtag import Hashtag
-
+from classes.Page import Page
 
 load_dotenv()
 
@@ -55,3 +53,40 @@ class Neo4jIntegration:
                 except Exception as e:
                     logging.error(f"Failed to execute: {query} with error: {e}")
         
+    def insert_hashtag(self, hashtag: Hashtag):
+        """
+        Insert or update a hashtag in the database.
+
+        Args:
+            hashtag (Hashtag): The Hashtag object containing data to insert or update.
+        """
+        query = """
+        MERGE (t:Tag {name: $name})
+        ON CREATE SET 
+            t.totalCount = $count,
+            t.firstUsed = $first_appearance_date,
+            t.lastUsed = $last_appearance_date
+        ON MATCH SET
+            t.totalCount = t.totalCount + $count,
+            t.lastUsed = $last_appearance_date
+        WITH t
+        UNWIND $sources AS source_path
+        MERGE (s:Source {path: source_path})
+        MERGE (t)-[:APPEARS_IN]->(s)
+        """
+
+        parameters = {
+            "name": hashtag.name,
+            "count": hashtag.count,
+            "first_appearance_date": hashtag.first_appearance_date.isoformat() if hashtag.first_appearance_date else None,
+            "last_appearance_date": hashtag.last_appearance_date.isoformat() if hashtag.last_appearance_date else None,
+            "sources": [source.path for source in hashtag.sources],
+        }
+
+        with self.driver.session() as session:
+            try:
+                session.run(query, parameters)
+                logging.info(f"Inserted/Updated hashtag: {hashtag.name}")
+            except Exception as e:
+                logging.error(f"Failed to insert/update hashtag: {hashtag.name} with error: {e}")
+
