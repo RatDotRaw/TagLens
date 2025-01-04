6083e5e5202048e0695fe9d674b8c7e69e3b4e06
Rat. <33757638+RatDotRaw@users.noreply.github.com>
2025-01-04 16:31:32 +0100

    WIP(webui): summary section pt.2

--- website/static/js/notes.js
+++ website/static/js/notes.js
@@ -4,6 +4,8 @@ all textarea id's
 n = notes 
 j = journal
 h = hashtag
+s = sentiment
+m = mixex (notes + journal + ...)
 
 n-h-longegevity
 n-h-uses-months
@@ -23,17 +25,21 @@ document.addEventListener("DOMContentLoaded", () => {
 const n_h_longegevity = document.getElementById("n-h-longegevity")
 const n_h_uses_months = document.getElementById("n-h-uses-months")
 const n_h_uses_split = document.getElementById("n-h-uses-split")
+const n_m_longevity = document.getElementById("n-m-longevity")
+const n_s_evolution = document.getElementById("n-s-evolution")
 
 function render_conclusion() {
     // get ctx of the view elements
     const note_view1 = document.getElementById("note-view1");
     const note_view2 = document.getElementById("note-view2");
     const note_view3 = document.getElementById("note-view3");
+    const note_view4 = document.getElementById("note-view4");
+    const note_view5 = document.getElementById("note-view5");
 
     // set the text area's value to the view element
     note_view1.textContent = n_h_longegevity.value ? n_h_longegevity.value : "No notes here...";
     note_view2.textContent = n_h_uses_months.value ? n_h_uses_months.value : "No notes here...";
     note_view3.textContent = n_h_uses_split.value ? n_h_uses_split.value : "No notes here...";
-
-
+    note_view4.textContent = n_m_longevity.value ? n_m_longevity.value : "No notes here...";
+    note_view5.textContent = n_s_evolution.value ? n_s_evolution.value : "No notes here...";
 }
